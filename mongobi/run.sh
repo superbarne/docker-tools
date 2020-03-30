6dd82f9bd4d9f0bcd622c14f59d9f121523393d9
Barne Jürgensen <barne.juergensen@codeanker.de>
2020-03-30 15:16:36 +0200

    update mongobi

--- mongobi/run.sh
+++ mongobi/run.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
 echo $MONGO_URI
-
-mongodrdl -h $MONGO_URI -d $MONGO_DB > schema.drdl
-
-mongoSQLDParams="--addr 0.0.0.0 --schema schema.drdl --mongo-uri $MONGO_URI"
+mongodrdl --uri $MONGO_URI > schema.drdl
+mongoSQLDParams="--addr 0.0.0.0 --schema schema.drdl --mongo-uri $MONGO_URI_WITHOUT_DATABASE"
 
 if [ -n "$MONGO_AUTH" ]; then
     mongoSQLDParams="${mongoSQLDParams} --auth"
